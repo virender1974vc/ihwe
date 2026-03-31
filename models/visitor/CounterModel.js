65bd96a8222c1355ed428c0091fb17a35061dc28
Manish <officialmanishsirohi.01@gmail.com>
2026-03-31 16:58:26 +0530

    changes done

--- models/visitor/CounterModel.js
+++ models/visitor/CounterModel.js
@@ -1,11 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const CounterSchema = new mongoose.Schema({
-//   type: { type: String, required: true, unique: true },
-//   seq: { type: Number, default: 0 },
-// });
-
-// export default mongoose.model("Counter", CounterSchema);
 const mongoose = require("mongoose");
 const { secondaryDB } = require("../../config/secondaryDb");
 const CounterSchema = new mongoose.Schema({
@@ -14,6 +6,6 @@ const CounterSchema = new mongoose.Schema({
 });
 
 const Counter =
-  secondaryDB.models.Counter || secondaryDB.model("Counter", CounterSchema);
+  secondaryDB.models.VisitorCounter || secondaryDB.model("VisitorCounter", CounterSchema);
 
 module.exports = Counter;
