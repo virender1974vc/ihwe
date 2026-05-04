ade599bbf6ad1288b3211ca8e796f955aa6a20e3
Vansh Chaudhary <vansh.2002vc@gmail.com>
2026-05-04 19:00:36 +0530

    04 backendd

--- models/LegalPolicy.js
+++ models/LegalPolicy.js
@@ -5,7 +5,7 @@ const legalPolicySchema = new mongoose.Schema({
         type: String, 
         required: true, 
         unique: true,
-        enum: ['privacy-policy', 'terms-of-service', 'refund-policy'],
+        enum: ['privacy-policy', 'terms-of-service', 'refund-policy', 'payment-policy', 'cancellation-policy'],
         trim: true 
     },
     title: {
